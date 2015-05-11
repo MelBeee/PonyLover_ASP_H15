b548ee1261be16b7952480e16de18ff0df2ffbed
Melissa <201029426@P104-12.clg.priv>
2015-05-11 16:25:17 -0400

    Galerie Photo commencer. Inscription cheval presque fini

--- PoneyLover3.0/PoneyLover3.0/Controllers/HomeController.cs
+++ PoneyLover3.0/PoneyLover3.0/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,7 +26,20 @@ namespace PoneyLover3._0.Controllers
 
       public ActionResult Gestion()
       {
-         return View(); 
+         return View();
+      }
+
+      [HttpPost]
+      public ActionResult Upload(HttpPostedFileBase file)
+      {
+         if (file != null && file.ContentLength > 0)
+         {
+            var fileName = Path.GetFileName(file.FileName);
+            var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+            file.SaveAs(path);
+         }
+
+         return RedirectToAction("UploadDocument");
       }
    }
 }
