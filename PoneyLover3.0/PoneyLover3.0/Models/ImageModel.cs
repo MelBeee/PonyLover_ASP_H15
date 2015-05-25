fb40ce6e8ce3102df9d8d0b6faddbd91ed96efb0
Melissa <201029426@P104-03.clg.priv>
2015-05-25 11:59:05 -0400

    Banner Dynamique :+1:

---
+++ PoneyLover3.0/PoneyLover3.0/Models/ImageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoneyLover3._0.Models
+{
+   public class ImageModel
+   {
+      public int IDImage { get; set; }
+
+      public ImageModel()
+      {
+         IDImage = 1;
+         Refresh();
+      }
+
+      public void Refresh()
+      {
+         IDImage = GetAleatoire();
+      }
+
+      public int GetAleatoire()
+      {
+         Random rdn = new Random();
+         int math = rdn.Next(1, 10);
+
+
+         return math;
+      }
+   }
+}
